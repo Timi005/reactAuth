dc0a5581c8c05ad259cff2e9a498eea0ff590056
Timii <akinstimilehin@gmail.com>
2022-11-16 12:44:31 +0100

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import Signup from "./Signup";
+import React from "react";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import ForgotPassword from "./ForgotPassword";
+import Updateprofile from "./Updateprofile";
+
+//prettier.ignore
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-300" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              ></Route>{" "}
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/update-profile" element={<Updateprofile />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
